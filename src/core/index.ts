125b2d3d97494ebd0ce139812602b701964cecfb
230055Fawwaz <evan.areefa@gmail.com>
2026-09-15 21:14:40 +0700

    Feat: Add entry points

---
+++ src/core/index.ts
@@ -0,0 +1,13 @@
+// ==========================================
+// Nama File:          index.ts
+// Deskripsi File:     Entry point bagi core
+// Penulis File:       Fawwaz Yaqzhan & Google Antigravity
+// Tanggal Pembuatan:  15-09-2026
+// Tanggal Pembaruan:  15-09-2026
+// Catatan:
+//   - Ekspor logika, algoritma dan struktur data core
+// ==========================================
+
+
+export * from "./MathUtils.js";
+export * from "./Vector2.js";
---
+++ src/index.ts
@@ -0,0 +1,12 @@
+// ==========================================
+// Nama File:          index.ts
+// Deskripsi File:     Entry point bagi library
+// Penulis File:       Fawwaz Yaqzhan & Google Antigravity
+// Tanggal Pembuatan:  15-09-2026
+// Tanggal Pembaruan:  15-09-2026
+// Catatan:
+//   - Ekspor isi library keluar
+// ==========================================
+
+
+export * from "./core/index.js";
